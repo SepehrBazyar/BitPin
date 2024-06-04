a2fe0979c981a1ed545f342b0744b0abff937be1
Sepehr <bazyar.sepehr@gmail.com>
2024-06-04 13:51:19 +0330

    feat: added configurations of pre commit and ruff

--- content/utils.py
+++ content/utils.py
@@ -2,5 +2,10 @@ from django.core.exceptions import ValidationError
 
 
 def validate_score(value: int):
+    """Validate score between 0 and 5
+
+    :param value: integer score number
+    :raises ValidationError: if score is invalid
+    """
     if not 0 <= value <= 5:
         raise ValidationError(f'Score must be between 0 and 5. Given: {value}')
